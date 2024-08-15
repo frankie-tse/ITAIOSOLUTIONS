c9853ee707cc6abec5418a7e4ffe96253d3a5643
Frankie Tse <tech.ftse@gmail.com>
2024-08-15 11:57:56 -0400

    ads

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const GetStarted = () => {
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+    
   return (
     <div className="bg-cyan-50 p-8 text-center rounded-lg w-full">
       <h3 className="text-2xl font-bold mb-4">Get Started With IT AIO Solutions Today</h3>
