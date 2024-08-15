b0934bcb47e02193391193968a9ea0055977f3ad
Frankie Tse <tech.ftse@gmail.com>
2024-08-15 11:54:06 -0400

    google ad tags

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -5,7 +5,7 @@ const GetStarted = () => {
   return (
     <div className="bg-cyan-50 p-8 text-center rounded-lg w-full">
       <h3 className="text-2xl font-bold mb-4">Get Started With IT AIO Solutions Today</h3>
-      <Link to="/contact" className="inline-block mt-4 px-6 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300">
+      <Link to="/contact" onClick={scrollToTop} className="inline-block mt-4 px-6 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300">
         Contact Us
       </Link>
     </div>
